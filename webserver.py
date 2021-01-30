7f16cfdbf3444ea6e0c7eaa62474a61da7fc2088
lodrantk <katarina.lodrant@gmail.com>
2021-01-31 00:29:14 +0100

    added parameter hasheader to graphcsv

--- webserver.py
+++ webserver.py
@@ -42,6 +42,19 @@ def pngcsv(filename):
 def pdfcsv(filename):
     return static_file('{}.pdf'.format(filename), root='output')
 
+@get('/home_data')
+def homefunc():
+    return bottle.template("templates/home_data.tpl")
+
+@get('/graph_data/<filename>.png')
+def pngfunc(filename):
+    return static_file('{}.png'.format(filename), root='output')
+
+@get('/graph_data/<filename>.pdf')
+def pdffunc(filename):
+    return static_file('{}.pdf'.format(filename), root='output')
+
+
 @get('/static/<filename>.jpg') #for background image
 def jpgimg(filename):
     return static_file('{}.jpg'.format(filename), root='static')
@@ -55,24 +68,50 @@ def graph():
     legend = request.forms.get("legend")
     xlabel = request.forms.get("xlabel")
     ylabel = request.forms.get("ylabel")
-    color = request.forms.get("color")
-    linewidth = request.forms.get("linewidth")
-    fontsize = request.forms.get("fontsize")
+    linecolor = request.forms.get("linecolor")
+    linewidth = request.forms.get("linewidthInput")
+    fontsize = request.forms.get("fontsizeInput")
     linestyle = request.forms.get("linestyle")
     grid = request.forms.get("grid")
     usetex = request.forms.get("usetex")
-    marker = request.forms.get("marker")
 
     name = graph_func(function, xmin, xmax, title, legend, xlabel, ylabel,
-                      color, linewidth, fontsize, linestyle, grid, usetex, marker)
+                      linecolor, linewidth, fontsize, linestyle, grid, usetex)
 
-    return bottle.template("templates/graph_func.tpl", name=name, function=function, title=title, xmin=xmin, xmax=xmax, legend=(legend == "on"), ylabel=ylabel, xlabel=xlabel, color=color, linewidth=linewidth, fontsize=fontsize, linestyle=linestyle, grid=grid, usetex=usetex, marker=marker)
+    return bottle.template("templates/graph_func.tpl", name=name, function=function, title=title, xmin=xmin, xmax=xmax, legend=(legend == "on"), ylabel=ylabel, xlabel=xlabel, linecolor=linecolor, linewidth=linewidth, fontsize=fontsize, linestyle=linestyle, grid=grid, usetex=usetex)
 
 
 @bottle.post('/graph_csv')
 def graphcsv():
     uploadfile = request.files.get('uploadfile')
+    hasheader = request.forms.get("hasheader")
 
+    title = request.forms.get("title")
+    xlabel = request.forms.get("xlabel")
+    ylabel = request.forms.get("ylabel")
+
+    fontsize = request.forms.get("fontsizeInput")
+    grid = request.forms.get("grid")
+    usetex = request.forms.get("usetex")
+    legend = request.forms.get("legend")
+
+    linestyle = request.forms.get("linestyle")
+    linecolor = request.forms.get("linecolor")
+    linewidth = request.forms.get("linewidthInput")
+
+    marker = request.forms.get("marker")
+    markercolor = request.forms.get("markercolor")
+
+    linefit = request.forms.get("linefit")
+    fitcolor = request.forms.get("fitcolor")
+
+    name = graph_csv(uploadfile, title, xlabel, ylabel, fontsize, grid, usetex, legend,
+                     linestyle, linecolor, linewidth, marker, markercolor, linefit, fitcolor, hasheader)
+
+    return bottle.template("templates/graph_csv.tpl", name=name, title=title, xlabel=xlabel, ylabel=ylabel, fontsize=fontsize, grid=grid, usetex=usetex, legend=legend, linestyle=linestyle, linewidth=linewidth, linecolor=linecolor, marker=marker, markercolor=markercolor, linefit=linefit, fitcolor=fitcolor, hasheader=hasheader)
+
+@bottle.post('/graph_data')
+def graphdata():
     title = request.forms.get("title")
     xlabel = request.forms.get("xlabel")
     ylabel = request.forms.get("ylabel")
@@ -92,7 +131,7 @@ def graphcsv():
     linefit = request.forms.get("linefit")
     fitcolor = request.forms.get("fitcolor")
 
-    name = graph_csv(uploadfile, title, xlabel, ylabel, fontsize, grid, usetex, legend,
+    name = graph_csv(title, xlabel, ylabel, fontsize, grid, usetex, legend,
                      linestyle, linecolor, linewidth, marker, markercolor, linefit, fitcolor)
 
     return bottle.template("templates/graph_csv.tpl", name=name, title=title, xlabel=xlabel, ylabel=ylabel, fontsize=fontsize, grid=grid, usetex=usetex, legend=legend, linestyle=linestyle, linewidth=linewidth, linecolor=linecolor, marker=marker, markercolor=markercolor, linefit=linefit, fitcolor=fitcolor)
