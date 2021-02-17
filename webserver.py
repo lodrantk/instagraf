7d3d93023a910ab5a096fbfc4f46bbbfeea908f5
lodrantk <katarina.lodrant@gmail.com>
2021-02-17 16:43:26 +0100

    added error-detection: return warning and error_image

--- webserver.py
+++ webserver.py
@@ -6,7 +6,6 @@ from graph_data import graph_data
 from pandas import read_csv
 import os
 
-
 # create directory 'output' for saved figures
 if not os.path.exists('output'):
     os.makedirs('output')
@@ -25,12 +24,17 @@ def homefunc():
 
 @get('/graph_func/<filename>.png')
 def pngfunc(filename):
-    return static_file('{}.png'.format(filename), root='output')
+    if filename == "error_image":
+        return static_file('{}.png'.format(filename), root='static')
+    else:
+        return static_file('{}.png'.format(filename), root='output')
 
 @get('/graph_func/<filename>.pdf')
 def pdffunc(filename):
-    return static_file('{}.pdf'.format(filename), root='output')
-
+    if filename == "error_image":
+        return static_file('{}.pdf'.format(filename), root='static')
+    else:
+        return static_file('{}.pdf'.format(filename), root='output')
 
 @get('/home_csv')
 def homecsv():
@@ -38,11 +42,17 @@ def homecsv():
 
 @get('/graph_csv/<filename>.png')
 def pngcsv(filename):
-    return static_file('{}.png'.format(filename), root='output')
+    if filename == "error_image":
+        return static_file('{}.png'.format(filename), root='static')
+    else:
+        return static_file('{}.png'.format(filename), root='output')
 
 @get('/graph_csv/<filename>.pdf')
 def pdfcsv(filename):
-    return static_file('{}.pdf'.format(filename), root='output')
+    if filename == "error_image":
+        return static_file('{}.pdf'.format(filename), root='static')
+    else:
+        return static_file('{}.pdf'.format(filename), root='output')
 
 @get('/home_data')
 def homedata():
@@ -50,12 +60,17 @@ def homedata():
 
 @get('/graph_data/<filename>.png')
 def pngdata(filename):
-    return static_file('{}.png'.format(filename), root='output')
+    if filename == "error_image":
+        return static_file('{}.png'.format(filename), root='static')
+    else:
+        return static_file('{}.png'.format(filename), root='output')
 
 @get('/graph_data/<filename>.pdf')
 def pdfdata(filename):
-    return static_file('{}.pdf'.format(filename), root='output')
-
+    if filename == "error_image":
+        return static_file('{}.pdf'.format(filename), root='static')
+    else:
+        return static_file('{}.pdf'.format(filename), root='output')
 
 @get('/static/<filename>.jpg') #for background image
 def jpgimg(filename):
@@ -76,22 +91,23 @@ def graph():
     linestyle = request.forms.get("linestyle")
     grid = request.forms.get("grid")
     usetex = request.forms.get("usetex")
-
-    name = graph_func(function, xmin, xmax, title, legend, xlabel, ylabel,
-                      linecolor, linewidth, fontsize, linestyle, grid, usetex)
-
-    return bottle.template("templates/graph_func.tpl", name=name, function=function, title=title, xmin=xmin, xmax=xmax, legend=(legend == "on"), ylabel=ylabel, xlabel=xlabel, linecolor=linecolor, linewidth=linewidth, fontsize=fontsize, linestyle=linestyle, grid=grid, usetex=usetex)
-
-
+    try:
+        name = graph_func(function, xmin, xmax, title, legend, xlabel, ylabel,
+                        linecolor, linewidth, fontsize, linestyle, grid, usetex)
+        return bottle.template("templates/graph_func.tpl", errormessage="", name=name, function=function, title=title, xmin=xmin, xmax=xmax, legend=(legend == "on"), ylabel=ylabel, xlabel=xlabel, linecolor=linecolor, linewidth=linewidth, fontsize=fontsize, linestyle=linestyle, grid=grid, usetex=usetex)
+    except Exception:
+        errormessage = "Vnešeni podatki niso ustrezni."
+        return bottle.template("templates/graph_func.tpl", errormessage=errormessage, name="error_image", function=function, title=title, xmin=xmin, xmax=xmax, legend=(legend == "on"), ylabel=ylabel, xlabel=xlabel, linecolor=linecolor, linewidth=linewidth, fontsize=fontsize, linestyle=linestyle, grid=grid, usetex=usetex)
+    
 @bottle.post('/graph_csv')
 def graphcsv():
     uploadfile = request.files.get('uploadfile')
     hasheader = request.forms.get("hasheader")
-
+    """
     name, ext = os.path.splitext(uploadfile.filename)
     if ext not in ('.csv'):
         return 'Izbrana datoteka ni ustrezna.'
-
+    """
     title = request.forms.get("title")
     xlabel = request.forms.get("xlabel")
     ylabel = request.forms.get("ylabel")
@@ -111,24 +127,20 @@ def graphcsv():
     linefit = request.forms.get("linefit")
     fitcolor = request.forms.get("fitcolor")
 
-    name = graph_csv(uploadfile, title, xlabel, ylabel, fontsize, grid, usetex, legend,
+    try:
+        name = graph_csv(uploadfile, title, xlabel, ylabel, fontsize, grid, usetex, legend,
                      linestyle, linecolor, linewidth, marker, markercolor, linefit, fitcolor, hasheader)
+        return bottle.template("templates/graph_csv.tpl", errormessage="error_image", name=name, title=title, xlabel=xlabel, ylabel=ylabel, fontsize=fontsize, grid=grid, usetex=usetex, legend=legend, linestyle=linestyle, linewidth=linewidth, linecolor=linecolor, marker=marker, markercolor=markercolor, linefit=linefit, fitcolor=fitcolor, hasheader=hasheader)
+    except Exception:
+        errormessage="Izbrana datoteka ni ustrezna."
+        return bottle.template("templates/graph_csv.tpl", errormessage=errormessage, name="error_image", title=title, xlabel=xlabel, ylabel=ylabel, fontsize=fontsize, grid=grid, usetex=usetex, legend=legend, linestyle=linestyle, linewidth=linewidth, linecolor=linecolor, marker=marker, markercolor=markercolor, linefit=linefit, fitcolor=fitcolor, hasheader=hasheader)
 
-    return bottle.template("templates/graph_csv.tpl", name=name, title=title, xlabel=xlabel, ylabel=ylabel, fontsize=fontsize, grid=grid, usetex=usetex, legend=legend, linestyle=linestyle, linewidth=linewidth, linecolor=linecolor, marker=marker, markercolor=markercolor, linefit=linefit, fitcolor=fitcolor, hasheader=hasheader)
 
 @bottle.post('/graph_data')
 def graphdata():
 
-    print(request.forms)
-
     plotdatax = request.forms.get("plotdatax")
     plotdatay = request.forms.get("plotdatay")
-
-    x = list(map(float, plotdatax.split(",")))
-    y = list(map(float, plotdatay.split(",")))
-
-    if len(x) != len(y) or len(x) == 0:
-        return "Vnešeni podatki niso ustrezni."
 
     title = request.forms.get("title")
     xlabel = request.forms.get("xlabel")
@@ -154,23 +166,12 @@ def graphdata():
 
     hasxerror= request.forms.get("hasxerror")
     xerror = request.forms.get("xerror")
-
-    if hasxerror == "on":
-        xerr = list(map(float, xerror.split(",")))
-        if len(xerr) != len(x):
-            return "Vnešeni podatki niso ustrezni."
-    else:
-        xerr=None
-    if hasyerror == "on":
-        yerr = list(map(float, yerror.split(",")))
-        if len(yerr) != len(x):
-            return "Vnešeni podatki niso ustrezni."
-    else:
-        yerr=None
-
-    name = graph_data(x, y, hasxerror, xerr, hasyerror, yerr, title, xlabel, ylabel, fontsize, grid, usetex, legend, linestyle, linecolor, linewidth, marker, markercolor, linefit, fitcolor)
-
-    return bottle.template("templates/graph_data.tpl", hasxerror=hasxerror, hasyerror=hasyerror, xerror=xerror, yerror=yerror, plotdatay=plotdatay, plotdatax=plotdatax, name=name, title=title, xlabel=xlabel, ylabel=ylabel, fontsize=fontsize, grid=grid, usetex=usetex, legend=legend, linestyle=linestyle, linewidth=linewidth, linecolor=linecolor, marker=marker, markercolor=markercolor, linefit=linefit, fitcolor=fitcolor)
-
+    
+    try:
+        name = graph_data(plotdatax, plotdatay, hasxerror, xerror, hasyerror, yerror, title, xlabel, ylabel, fontsize, grid, usetex, legend, linestyle, linecolor, linewidth, marker, markercolor, linefit, fitcolor)
+        return bottle.template("templates/graph_data.tpl", errormessage="", hasxerror=hasxerror, hasyerror=hasyerror, xerror=xerror, yerror=yerror, plotdatay=plotdatay, plotdatax=plotdatax, name=name, title=title, xlabel=xlabel, ylabel=ylabel, fontsize=fontsize, grid=grid, usetex=usetex, legend=legend, linestyle=linestyle, linewidth=linewidth, linecolor=linecolor, marker=marker, markercolor=markercolor, linefit=linefit, fitcolor=fitcolor)
+    except Exception:
+        errormessage="Vnešeni podatki niso ustrezni."
+        return bottle.template("templates/graph_data.tpl", errormessage=errormessage, hasxerror=hasxerror, hasyerror=hasyerror, xerror=xerror, yerror=yerror, plotdatay=plotdatay, plotdatax=plotdatax, name="error_image", title=title, xlabel=xlabel, ylabel=ylabel, fontsize=fontsize, grid=grid, usetex=usetex, legend=legend, linestyle=linestyle, linewidth=linewidth, linecolor=linecolor, marker=marker, markercolor=markercolor, linefit=linefit, fitcolor=fitcolor)
 
 bottle.run(debug=True, host="0.0.0.0", port=80, reloader=True)
